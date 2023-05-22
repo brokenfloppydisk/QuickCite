ab19fa846bf02328bab332a5fb6baa962d1c6b05
Zachary Miller <93176330+londonwafflez@users.noreply.github.com>
2023-05-22 11:25:55 -0700

    Update toMLA and toAPA

--- src/main/java/lib/publication/Book.java
+++ src/main/java/lib/publication/Book.java
@@ -29,11 +29,16 @@ public class Book implements Publication {
     }
 
     public String toAPA() {
+//         https://www.grammarly.com/citations/apa
+//         Last name, First Initial. Middle Initial. (Year). Title. Source Info.
+//         return String.format("%s, %s. (%d). %s. %s", "Huang", "K", this.publishDate.getYear(), this.title, "idk");
         return "";
     }
 
     public String toMLA() {
-
+//      https://www.grammarly.com/citations/mla
+//      Last name, First name. Title of Source. Other contributors, Version, Publisher, Year.
+//         return String.format("%s, %s. %s. %d", "Hunag", "K", this.title, this.publishDate.getYear());
         return "";
     }
 
