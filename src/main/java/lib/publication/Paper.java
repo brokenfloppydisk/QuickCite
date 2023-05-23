985da6fe7093af85f3388b7d20db6b9756ad2a1e
bbq27 <133260826+bbq27@users.noreply.github.com>
2023-05-23 11:51:02 -0700

    Update Paper.java

--- src/main/java/lib/publication/Paper.java
+++ src/main/java/lib/publication/Paper.java
@@ -47,12 +47,67 @@ public class Paper implements Publication {
     public String toAPA() {
         // Author, A. A., & Author, B. B. (Date of publication). Article Title. Journal Title, Vol. #(Issue # if available), page range. doi #
         String str = String.format("%s, %s",
-            author.getAuthors
-        )
-        return "";
+            authors.getAuthors(0).getLastName(),
+            authors.getAuthors(0).getFirstInitial());
+
+        if (authors.size() > 1) {
+            for (int i = 1; i < authors.size(); i++) {
+                if (i == authors.size() - 1){
+                    str = String.format("%s, & %s, %s.",
+                        str,
+                        authors.get(i).getLastName();
+                        authors.get(i).getFirstInitial());
+                } else {
+                    str = String.format("%s, %s, %s.")
+                        str,
+                        authors.get(i).getLastName(),
+                        authors.get(i).getFirstInitial());
+                }
+            }
+        }
+
+        str = String.format("%s (%s), %s, %s",
+            str,
+            "Year"
+            this.title,
+            "Journal Title");
+            
+        return str;
     }
 
     public String toMLA() {
+
+// Last, First. "Title" Journal Title, vol #, no #, Month Year, pp. #-#, DOI link  
+
+        String str = String.format("%s, %s.",
+            authors.get(0).getLastName();
+            authors.get(0).getFirstName());
+        
+        if (authors.size() > 1) {
+            for (int i = 1; i < authors.size(); i++) {
+                if (i == authors.size() - 1) {
+                    str = String.format("%s, & %s, %s",
+                        str,
+                        authors.get(i).getLastName(),
+                        authors.get(i).getFirstName());
+                } else {
+                    str = String.format("%s, & %s, %s",
+                        str,
+                        authors.get(i).getLastName(),
+                        authors.get(i).getFirstName());
+                }
+            }
+        }
+
+        str = String.format("%s, \"%s\", %s, %s, %s, %s, %s",
+            str,
+            this.title,
+            "Journal Title",
+            "Vol #",
+            "No #",
+            "Month",
+            "Year"
+
 
         return "";
     }
