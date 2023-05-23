ceed436a4443ce999e5fb34f3091ae2bfe5ee837
Zachary Miller <93176330+londonwafflez@users.noreply.github.com>
2023-05-23 11:16:33 -0700

    Update toMLA and toAPA for Book

--- src/main/java/lib/publication/Book.java
+++ src/main/java/lib/publication/Book.java
@@ -31,15 +31,65 @@ public class Book implements Publication {
     public String toAPA() {
 //         https://www.grammarly.com/citations/apa
 //         Last name, First Initial. Middle Initial. (Year). Title. Source Info.
-//         return String.format("%s, %s. (%d). %s. %s", "Huang", "K", this.publishDate.getYear(), this.title, "idk");
-        return "";
+        String str = String.format("%s, %s.", 
+            authors.get(0).getLastName(), 
+            authors.get(0).getFirstInitial());
+        
+        if (authors.size() > 1) {
+            for (int i = 1; i < authors.size(); i++) {
+                if (i == authors.size() - 1) {
+                    str = String.format("%s, & %s, %s.", 
+                        str,
+                        authors.get(i).getLastName(), 
+                        authors.get(i).getFirstInitial());
+                } else {
+                    str = String.format("%s, %s, %s.", 
+                        str,
+                        authors.get(i).getLastName(), 
+                        authors.get(i).getFirstInitial());
+                }
+            }
+        }
+
+        String.format("%s (%s). %s. %s", 
+            str,
+            "Year",
+            this.title,
+            "Publisher");
+
+        return str;
     }
 
     public String toMLA() {
 //      https://www.grammarly.com/citations/mla
 //      Last name, First name. Title of Source. Other contributors, Version, Publisher, Year.
 //         return String.format("%s, %s. %s. %d", "Hunag", "K", this.title, this.publishDate.getYear());
-        return "";
+        String str = String.format("%s, %s.", 
+            authors.get(0).getLastName(), 
+            authors.get(0).getFirstName());
+        
+        if (authors.size() > 1) {
+            for (int i = 1; i < authors.size(); i++) {
+                if (i == authors.size() - 1) {
+                    str = String.format("%s, & %s, %s", 
+                        str,
+                        authors.get(i).getLastName(), 
+                        authors.get(i).getFirstName());
+                } else {
+                    str = String.format("%s, %s, %s", 
+                        str,
+                        authors.get(i).getLastName(), 
+                        authors.get(i).getFirstName());
+                }
+            }
+        }
+        str = String.format("%s, %s, %s, %s", 
+            str,
+            this.title,
+            "Publisher"
+            "Year");
+
+        return str;
     }
 
     public String getTitle() {
